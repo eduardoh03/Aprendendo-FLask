d6774c3b479bfb608d5a9952db75eca89da003f9
Eduardo Henrique <eduhenriquesrs@gmail.com>
2022-03-21 13:41:48 -0300

    Configurado o banco de dados, criado models e a migration de tarefa .

--- api/views/tarefa_views.py
+++ api/views/tarefa_views.py
@@ -3,7 +3,7 @@ from api import api
 
 class TarefaList(Resource):# List é adicionado em metodos que não necessitao de parametros GET e POST
     def get(self):
-        return "Olá Mundo"
+        return "Ola Mundo"
 api.add_resource(TarefaList, '/tarefas')
 
 # class TarefaDetail(Resource):# Detail é adicionado em metodos que necessitao de parametros PUT e DELETE
