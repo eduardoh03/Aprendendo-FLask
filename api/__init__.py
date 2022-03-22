d128ebb87dfb086ea82d50afacbffc4d7fab0d1a
Eduardo Henrique <eduhenriquesrs@gmail.com>
2022-03-22 10:55:07 -0300

    Criado Schemas de Tarefa.

--- api/__init__.py
+++ api/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+ma = Marshmallow(app) #deve ser adicionado depois do SQLAlchemy para fazer a tradução dos dados python <-> JSON
 migrate = Migrate(app,db)
 
+
 api = Api(app)
+
 from .views import tarefa_views
 from .models import tarefa_model
