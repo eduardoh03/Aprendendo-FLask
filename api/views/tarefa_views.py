93225d530b01a7a891410f427f0a02b7f21f6e3c
Eduardo Henrique <eduhenriquesrs@gmail.com>
2022-03-22 21:53:34 -0300

    Get by Id de Tarefas criado.

--- api/views/tarefa_views.py
+++ api/views/tarefa_views.py
@@ -4,13 +4,15 @@ from ..schemas import tarefa_schemas
 from flask import request, make_response, jsonify
 from ..entidades import tarefa
 from ..services import tarefa_service
-#A view está responsável por tratar os dados das requisições
 
-class TarefaList(Resource):# List é adicionado em metodos que não necessitao de parametros GET e POST
+
+# A view está responsável por tratar os dados das requisições
+
+class TarefaList(Resource):  # List é adicionado em metodos que não necessitao de parametros GET e POST
 
     def get(self):
         tarefas = tarefa_service.listar_tarefas()
-        #many é necessario porque várias tarefas vão ser passadas pelo Schema
+        # many é necessario porque várias tarefas vão ser passadas pelo Schema
         ts = tarefa_schemas.TarefaSchema(many=True)
         return make_response(ts.jsonify(tarefas), 200)
 
@@ -26,6 +28,25 @@ class TarefaList(Resource):# List é adicionado em metodos que não necessitao d
             tarefa_nova = tarefa.Tarefa(titulo=titulo, descricao=descricao, data_expiracao=data_expiracao)
             result = tarefa_service.cadastrar_tarefa(tarefa_nova)
             return make_response(ts.jsonify(result), 201)
-api.add_resource(TarefaList, '/tarefas')
 
-# class TarefaDetail(Resource):# Detail é adicionado em metodos que necessitao de parametros PUT e DELETE
+
+class TarefaDetail(Resource):  # Detail é adicionado em metodos que necessitao de parametros PUT e DELETE
+
+    def get(self, id):
+        tarefa = tarefa_service.listar_tarefa_id(id)
+        if tarefa is None:
+            return make_response(jsonify("Tarefa nao encontrada."), 404)
+        else:
+            ts = tarefa_schemas.TarefaSchema()
+            return make_response(ts.jsonify(tarefa), 200)
+
+    def put(self, id):
+        pass
+
+    def delete(self, id):
+        pass
+
+
+# CRIADO A ROTA
+api.add_resource(TarefaList, '/tarefas')
+api.add_resource(TarefaDetail, '/tarefa/<int:id>')
