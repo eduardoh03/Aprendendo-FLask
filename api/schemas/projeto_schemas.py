b83c283e586d7a5248df019a849ba385fb82e0df
Eduardo Henrique <eduhenriquesrs@gmail.com>
2022-03-24 13:36:21 -0300

    Adicionado listagem de Tarefas nos Projetos.

--- api/schemas/projeto_schemas.py
+++ api/schemas/projeto_schemas.py
@@ -6,8 +6,9 @@ from marshmallow import fields
 class ProjetoSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = projeto_model.Projeto
-        fields = ("id", "nome", "descricao")
+        fields = ("id", "nome", "descricao", "tarefas")
         # load_instance = True
 
     nome = fields.String(required=True)
     descricao = fields.String(required=True)
+    tarefas = fields.List(fields.String)
